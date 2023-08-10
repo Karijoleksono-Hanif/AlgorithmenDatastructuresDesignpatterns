b34a1f1759ee4126bb0a6b6041b9cd0ed017b03e
Hanif Karijoleksono <karijoleksono-suwondo@hotmail.com>
2023-08-10 05:36:40 -0300

    new

--- src/DP_AbstractFactory/Location.java
+++ src/DP_AbstractFactory/Location.java
@@ -1,5 +1,30 @@
 package DP_AbstractFactory;
 
 enum Location implements javax.xml.stream.Location {
-    DEFAULT, USA, INDIA
+    DEFAULT, USA, INDIA;
+
+    @Override
+    public int getLineNumber() {
+        return 0;
+    }
+
+    @Override
+    public int getColumnNumber() {
+        return 0;
+    }
+
+    @Override
+    public int getCharacterOffset() {
+        return 0;
+    }
+
+    @Override
+    public String getPublicId() {
+        return null;
+    }
+
+    @Override
+    public String getSystemId() {
+        return null;
+    }
 }
